844ed63113805adef4c2d9bb88e083dd5479c34e
Carlos Anguiano <c.joseanguiano@gmail.com>
2017-06-04 00:46:17 -0500

    Using a for loop

---
+++ src/main.kt
@@ -0,0 +1,11 @@
+/**
+ * Created by carlosjoseanguiano on 04/06/17.
+ */
+
+fun main(args: Array<String>) {
+    val items = listOf("a", "b", "c")
+    for (item in items) {
+        println(item)
+    }
+
+}
